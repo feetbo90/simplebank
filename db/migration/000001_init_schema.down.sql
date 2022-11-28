ecf568967d61bca7bc04a4eca9ec2f7804760b6a
Angga Hasibuan <dickyhasibuan@Dickys-MacBook-Pro.local>
2022-11-28 14:27:19 +0700

    initial commit

---
+++ db/migration/000001_init_schema.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF exists entries;
+DROP TABLE IF exists accounts;
+DROP TABLE IF exists transfers;
+DROP TABLE IF exists orders;
---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,51 @@
+CREATE TABLE "accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "orders" (
+  "id" int PRIMARY KEY,
+  "user_id" int UNIQUE NOT NULL,
+  "status" varchar,
+  "created_at" varchar
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
+
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
+
+COMMENT ON COLUMN "orders"."created_at" IS 'When order created';
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
